2f170f1165c4d1b1e84547dea9261be23c44f859
ios3144 <ios3144@Mac-mini-de-caelum-8.local>
2013-01-16 19:30:26 -0200

    Initial Commit

---
+++ ContatosIP67/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios3144 on 16/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

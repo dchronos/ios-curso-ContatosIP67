d965c93a652c71c60ade6e05b4001c649c1d44f7
ios3144 <ios3144@Mac-mini-de-caelum-8.local>
2013-01-23 22:54:17 -0200

    Usando map, ex 15.4

---
+++ ContatosIP67/ContatosNoMapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContatosNoMapaViewController.h
+//  ContatosIP67
+//
+//  Created by ios3144 on 23/01/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContatosNoMapaViewController : UIViewController
+
+@end
